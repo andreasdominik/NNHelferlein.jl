aa22858fc935b297c50b3ce44a5957ee2be9c695
andreas.dominik <andreas.dominik@mni.thm.de>
2021-03-09 09:40:10 +0100

    padding mask adapted for multi-head attn

--- src/transformers.jl
+++ src/transformers.jl
@@ -51,17 +51,17 @@ end
 
 Make a padding mask; i.e. return an Array of type
 `KnetArray{Float32}` (or `Array{Float32}`) similar to `x` but with
-an additional 1st dimension of size 1 (this will represent the
-embedding or depth) after embedding,
+two additional dimension of size 1 in teh middle (this will represent the
+2nd seq_len and the number of heads) in multi-head attention
 and the
 value `1.0` at each position where `x` is `pad` and `0.0` otherwise.
 
 The function can be used for creating padding masks for attention
-mechanism.
+mechanisms.
 """
 function mk_padding_mask(x; pad=0)
 
-    return reshape(convert2KnetArray(x .== pad), 1,size(x)...)
+    return reshape(convert2KnetArray(x .== pad), size(x)[1],1,1,size(x)[2])
 end
 
 
