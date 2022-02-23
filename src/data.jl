20d0736139f76f7e9eca0653915dc0946419a6b5
Andreas Dominik <andreas.dominik@mni.thm.de>
2022-02-23 12:17:42 +0100

    dl exampledata minimised

--- src/data.jl
+++ src/data.jl
@@ -35,9 +35,9 @@ function download_example_data(path_name...; force=false)
 
     # download only if not already here:
     #
-    println(pname)
-    println("only: $fonly")
-    println(fname)
+    # println(pname)
+    # println("only: $fonly")
+    # println(fname)
     if !isfile(fname) || force
         mkpath(pname)
         println("Downloading example data: $fonly")
@@ -64,54 +64,3 @@ function download_pretrained(h5name, force=false)
 
     download_example_data("pretrained", h5name, force=force)
 end
-
-
-function download_example_data()
-
-    data_files = [
-        ["Images64", "car.png"],
-        ["Images64", "queen.png"],
-        ["Images64", "spock.png"],
-        ["Images64", "telephone.png"],
-        ["Images64", "tree.png"],
-
-        ["MIT-normal_sinus_rhythm", "16265.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "16272.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "16273.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "16420.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "16483.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "16539.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "16773.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "16786.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "16795.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "17052.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "17453.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "18177.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "18184.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "19088.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "19090.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "19093.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "19140.ecg.gz"],
-        ["MIT-normal_sinus_rhythm", "19830.ecg.gz"],
-
-        ["elecat", "cat.jpg"],
-        ["elecat", "elephant.jpg"],
-
-        ["elecat_224", "cat-224.jpg"],
-        ["elecat_224", "elephant-224.jpg"],
-
-        ["flowers", "daisy", "5547758_eea9edfd54_n_sqr.png"],
-        ["flowers", "daisy", "5673551_01d1ea993e_n_sqr.png"],
-        ["flowers", "daisy", "5673551_01d1ea993e_n_sqr_copy.png"],
-        ["flowers", "daisy", "5673551_01d1ea993e_n_sqr_copy_copy.png"],
-        
-        ["flowers", "rose", "22679076_bdb4c24401_m_sqr.png"],
-        ["flowers", "rose", "99383371_37a5ac12a3_n_sqr.png"],
-
-        ["flowers", "tulip", "11746080_963537acdc_sqr.png"],
-        ["flowers", "tulip", ""]
-    ]
-end
-
-
-#    https://github.com/andreasdominik/NNHelferlein.jl/raw/main/data/MIT-normal_sinus_rhythm/16265.ecg.gz
